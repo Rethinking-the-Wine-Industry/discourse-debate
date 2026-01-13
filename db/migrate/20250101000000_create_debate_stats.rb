1aa224b4e5e76a576fd38e4bb8b6c7a9a6bb26e9
Francis Mozart <francis.mozart@moises.ai>
2026-01-13 01:10:56 -0300

    fix build #3

--- db/migrate/20250101000000_create_debate_stats.rb
+++ db/migrate/20250101000000_create_debate_stats.rb
@@ -1,4 +1,4 @@
-class CreateDebateStats < ActiveRecord::Migration
+class CreateDebateStats < ActiveRecord::Migration[7.0]
   def change
     create_table :debate_stats do |t|
       t.integer :topic_id, null: false
@@ -7,7 +7,6 @@ class CreateDebateStats < ActiveRecord::Migration
       t.integer :against, default: 0
       t.timestamps null: false
     end
-
     add_index :debate_stats, :topic_id, unique: true
   end
 end
