7a2295c2d8d458b0afb3c0d6ddb0a9c172afffdb
Francis Mozart <francis.mozart@moises.ai>
2026-01-21 17:16:25 -0300

    register asset

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,8 @@
 # authors: Rethinking Wine
 # url: https://github.com/Rethinking-the-Wine-Industry/discourse-debates
 
+register_asset "config/settings.yml"
+
 enabled_site_setting :debate_enabled
 
 require_relative "lib/discourse_debates/engine"
