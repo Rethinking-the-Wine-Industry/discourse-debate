a1f1e4de5fd24fa9535a4d423c7e189835f46d39
Francis Mozart <francis.mozart@moises.ai>
2026-01-21 16:29:02 -0300

    plugin name

--- app/controllers/discourse_debates/debate_controller.rb
+++ app/controllers/discourse_debates/debate_controller.rb
@@ -2,10 +2,10 @@
 
 module ::DebatesModule
   class ExamplesController < ::ApplicationController
-    requires_plugin PLUGIN_NAME
+    requires_plugin "discourse-debates"
 
     def index
-      render json: { hello: "world" }
+      render json: { ok: true }
     end
   end
 end
