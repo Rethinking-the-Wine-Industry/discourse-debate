f7c0b4f83e154cf94fb7a397d83af2e006f4cff8
Francis Mozart <francis.mozart@moises.ai>
2026-01-21 16:45:36 -0300

    change model

---
+++ app/models/discourse_debates/debate.rb
@@ -0,0 +1,13 @@
+module DiscourseDebates
+  class Debate < ::ActiveRecord::Base
+    self.table_name = "debates"
+
+    belongs_to :topic
+
+    enum status: {
+      suggestion: 0,
+      open: 1,
+      closed: 2
+    }
+  end
+end
